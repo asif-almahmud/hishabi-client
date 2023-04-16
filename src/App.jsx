8e2bd8f73f342d4fd8df2ef5334733b840248047
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2023-04-16 22:50:42 +0600

    create basic folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <p className="text-2xl bg-slate-400">
+                Click on the Vite and React logos to learn more
+            </p>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/features/auth/authApi.js
@@ -0,0 +1,1 @@
+// API calls for authentication (login, logout, etc.)
